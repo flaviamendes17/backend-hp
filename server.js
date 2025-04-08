1293343840e5eec0255737143103daf0f2e37d32
Thiago Ferreira <thiago.rodrigues@sp.senai.br>
2025-04-08 08:41:49 -0300

    feat: adiciona funcionalidade de exportação de wizards para PDF

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes.js");
 const houseRoutes = require("./src/routes/houseRoutes.js");
+const reportRoutes = require("./src/routes/reportRoutes.js");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/api", wizardRoutes); 
 app.use("/api", houseRoutes); 
+app.use("/api", reportRoutes);
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
